d344b46c6535a3d1cd087562b692e1ae12ac4f81
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-10-23 23:15:37 +0300

    Create readtxt.cpp

---
+++ readtxt.cpp
@@ -0,0 +1,16 @@
+void loadMaze() {
+    std::ifstream mazeFile("maze.txt");
+    if (!mazeFile) {
+        std::cerr << "Error: Could not open maze file!" << std::endl;
+        exit(1); 
+    }
+
+    std::string line;
+    while (std::getline(mazeFile, line)) {
+        maze.push_back(line); 
+    }
+    mazeFile.close();
+
+
+
+}
